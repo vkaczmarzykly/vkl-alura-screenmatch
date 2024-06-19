9ffc12be8cd4481a792c005037eac18be1d777fd
vkaczmarzykly <vkaczmarzykly@gmail.com>
2024-06-18 22:44:13 -0300

    Commit Inicial

--- screenmatch/src/main/java/br/com/alura/vkl/screenmatch/service/ConverteDados.java
+++ screenmatch/src/main/java/br/com/alura/vkl/screenmatch/service/ConverteDados.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados {
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
